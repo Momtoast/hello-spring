3cde2c66fa9f5fec61e1a4671fde867c50fe3c88
Melissa Matos <melissamomtoast@gmail.com>
2022-08-22 18:09:08 -0400

    I hope there is a better way to add html for these things

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -35,9 +35,52 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" +
                 "<input type='text' name='name'>"+
+                "<select name='language'" +
+                "<option value=''>Please choose a language</option>" +
+                "<option value='English'>English</option>" +
+                "<option value='French'>French</option>" +
+                "<option value='Spanish'>Spanish</option>" +
+                "<option value='German'>German</option>" +
+                "<option value='Italian'>Italian</option>" +
+                "</select>" +
                 "<input type='submit' value='Greet Me'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("English")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("French")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("Italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("Spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("German")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
     }
 }
